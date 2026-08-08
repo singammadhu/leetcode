b66affec4bf3ce9ae1b60a4df2cac3890be2e682
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-08 20:03:13 +0530

    Time: 3 ms (100.00%) | Memory: 47.5 MB (58.07%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,20 +1,30 @@
 class Solution {
     public int minimumLength(String s) {
-      int n = s.length();
-      int l=0;
-      int r=n-1;
-      while(l<r&& s.charAt(l)==s.charAt(r)){
-           char ch = s.charAt(l); 
-          // Skip all consecutive ch from the left.
-          while (l < r && ch == s.charAt(l)) {
-           l++;
-           }
+        int n = s.length();
+        int l = 0;
+        int r = n - 1;
 
-           // Skip all consecutive ch from the right.
-           while (l <= r && ch == s.charAt(r)) {
-            r--;
-           }    
-      }
-      return r-l+1;  
+        while (l < r && s.charAt(l) == s.charAt(r)) {
+            char ch = s.charAt(l);
+
+            // skip consecutive ch from the left (original < style)
+            while (l < r && ch == s.charAt(l)) {
+                l++;
+            }
+
+            // skip consecutive ch from the right
+            while (l < r && ch == s.charAt(r)) {
+                r--;
+            }
+
+            // now handle the pure-run leftovers
+            if (l == r && s.charAt(l) == ch) {          // odd length left
+                return 0;
+            }
+            if (r == l + 1 && s.charAt(l) == s.charAt(r)) { // even length left
+                return 0;
+            }
+        }
+        return r - l + 1;
     }
 }
