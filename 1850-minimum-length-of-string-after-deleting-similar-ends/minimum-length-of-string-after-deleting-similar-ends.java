24a6591257c3f05ddc1b3a7d0f7bcefe9b80a425
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-08 20:03:43 +0530

    Time: 4 ms (72.31%) | Memory: 47.6 MB (22.91%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,30 +1,42 @@
 class Solution {
     public int minimumLength(String s) {
-        int n = s.length();
-        int l = 0;
-        int r = n - 1;
 
+        int l = 0;
+        int r = s.length() - 1;
+
+        /*
+         * l <= r because:
+         *
+         * l == r means one character is still remaining.
+         * We must allow this case to be processed.
+         *
+         * l < r  -> at least 2 characters remain
+         * l == r -> exactly 1 character remains
+         * l > r  -> no characters remain
+         */
         while (l < r && s.charAt(l) == s.charAt(r)) {
+
             char ch = s.charAt(l);
 
-            // skip consecutive ch from the left (original < style)
-            while (l < r && ch == s.charAt(l)) {
+            // Skip all consecutive ch from the left.
+            while (l <= r && s.charAt(l) == ch) {
                 l++;
             }
 
-            // skip consecutive ch from the right
-            while (l < r && ch == s.charAt(r)) {
+            // Skip all consecutive ch from the right.
+            while (l <= r && s.charAt(r) == ch) {
                 r--;
             }
-
-            // now handle the pure-run leftovers
-            if (l == r && s.charAt(l) == ch) {          // odd length left
-                return 0;
-            }
-            if (r == l + 1 && s.charAt(l) == s.charAt(r)) { // even length left
-                return 0;
-            }
         }
+
+        /*
+         * Distance between positions = r - l
+         *
+         * Number of elements from l through r = r - l + 1
+         *
+         * We need the number of remaining characters,
+         * so we use r - l + 1.
+         */
         return r - l + 1;
     }
 }
