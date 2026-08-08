a636f78fb13c845c81f22859a05635ba44295d24
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-08 20:07:42 +0530

    Time: 3 ms (100.00%) | Memory: 47.3 MB (75.70%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -4,22 +4,13 @@ class Solution {
         int l = 0;
         int r = s.length() - 1;
 
-        /*
-         * l <= r because:
-         *
-         * l == r means one character is still remaining.
-         * We must allow this case to be processed.
-         *
-         * l < r  -> at least 2 characters remain
-         * l == r -> exactly 1 character remains
-         * l > r  -> no characters remain
-         */
+    
         while (l < r && s.charAt(l) == s.charAt(r)) {
 
             char ch = s.charAt(l);
 
             // Skip all consecutive ch from the left.
-            while (l <= r && s.charAt(l) == ch) {
+            while (l < r && s.charAt(l) == ch) {
                 l++;
             }
 
@@ -29,14 +20,6 @@ class Solution {
             }
         }
 
-        /*
-         * Distance between positions = r - l
-         *
-         * Number of elements from l through r = r - l + 1
-         *
-         * We need the number of remaining characters,
-         * so we use r - l + 1.
-         */
         return r - l + 1;
     }
 }
