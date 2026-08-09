66860cac6d6f5fc256df05a6b8c5011efdbfcbe4
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-09 23:43:01 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (76.78%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,19 +1,50 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+
         int n = nums.length;
+
+        // Answer array to store squares in sorted order
         int[] ans = new int[n];
-        int l=0,r=n-1,i=n-1;
-        while(l<=r){
-            int ls=nums[l]*nums[l],rs=nums[r]*nums[r];
-            if(ls>rs){
-                ans[i]=ls;
-                l++;
-            }else {
-                ans[i]=rs;
+
+        // Left pointer -> leftmost candidate
+        int l = 0;
+
+        // Right pointer -> rightmost candidate
+        int r = n - 1;
+
+        // Next position to fill in the answer array
+        // We fill from the back because the largest square comes first.
+        int idx = n - 1;
+
+        // Process until all elements are used
+        while (l <= r) {
+
+            // Square of left and right elements
+            int leftSquare = nums[l] * nums[l];
+            int rightSquare = nums[r] * nums[r];
+
+            // The largest remaining square is always at one of the two ends.
+            if (leftSquare < rightSquare) {
+
+                // Place larger square at current answer position
+                ans[idx] = rightSquare;
+
+                // Left element has been used
                 r--;
+
+            } else {
+
+                // Place larger (or equal) square at current answer position
+                ans[idx] = leftSquare;
+
+                // Right element has been used
+                l++;
             }
-            i--;
+
+            // Move to the next position in the answer array
+            idx--;
         }
+
         return ans;
     }
 }
