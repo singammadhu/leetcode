7d7dbc5c9a84b088805b4457e00cafe57a31dd77
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-11 22:32:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (24.58%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,13 +1,21 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-       int k=0;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]!=val){
-            nums[k]=nums[i];
-            k++;
-        }
-       }
-       return k;
 
+        int write = 0;
+
+        for (int read = 0; read < nums.length; read++) {
+
+            if (nums[read] != val) {
+
+                // Keep this element by writing it
+                // at the next available position.
+                nums[write] = nums[read];
+
+                write++;
+            }
+        }
+
+        // write = number of elements remaining.
+        return write;
     }
 }
