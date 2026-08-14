eb43119c928324ec649226c92b8ebe534fbe3b0b
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-14 13:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (43.65%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,15 +1,28 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
 
-        int w =0;
-        for(int r=0;r<nums.length;r++){
-            if(nums[r]!=val){
-                nums[w]=nums[r];
-                w++;
-            }
-        }
-                    return w;
+        int w = 0;  // Worker: position where the next valid element goes.
+        int r =0;
+        // for (int r = 0; r < nums.length; r++) {
+        while(r<nums.length){
 
+            // r = Inspector: checks every place one by one.
+            if (nums[r] != val) {
+
+                // Not a dustbin → give this element to w.
+                nums[w] = nums[r];
+
+                // w placed the valid element, so move to next position.
+                w++;
+                r++;
+            }else{r++;}
+              
+            // If nums[r] == val:
+            // It is a dustbin → don't write anything.
+            // w stays here while r moves to find the next valid element.
+        }
+
+        // w = number of valid elements placed.
+        return w;
     }
-      
 }
