d8a860187b55528c24b53e670bdd3d0a786a0007
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-14 13:29:56 +0530

    Time: 1 ms (77.41%) | Memory: 47 MB (9.47%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -23,7 +23,7 @@ class Solution {
                 nums[w] = nums[r];
 
                 // Continue reading.
-                // r++;
+                // r++; is also ok
             }
         }
 
