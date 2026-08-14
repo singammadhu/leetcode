89630513dc6a2300ea653196cec974eaf9c66fbe
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-14 14:14:50 +0530

    Time: 2 ms (99.82%) | Memory: 48.9 MB (25.63%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -1,28 +1,49 @@
 class Solution {
-    public int[] sortArrayByParityII(int[] nums) {
-        int[] ans = new int[nums.length];
-
-        // Even numbers should go to even indices: 0, 2, 4...
-        int e = 0;
-
-        // Odd numbers should go to odd indices: 1, 3, 5...
-        int o = 1;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            // Place even number at the next available even index
-            if (nums[i] % 2 == 0) {
-                ans[e] = nums[i];
-                e += 2;
-            }
-
-            // Place odd number at the next available odd index
-            else {
-                ans[o] = nums[i];
-                o += 2;
-            }
+   public int[] sortArrayByParityII(int[] nums) {
+    int we=0;
+    int wo=1;
+    int r=0;
+    int n=nums.length;
+    int[] ans= new int[n];
+    
+    while(r<n){
+        if(nums[r]%2==0){
+            ans[we]=nums[r];
+            we=we+2;
+            r++;
+        }else{
+            ans[wo]=nums[r];
+            wo=wo+2;
+            r++;
         }
-
-        return ans;
     }
+    return ans;
+   }
+
 }
+ // public int[] sortArrayByParityII(int[] nums) {
+
+    //     // Start from the first odd index
+    //     int odd = 1;
+
+    //     // Check every even index
+    //     for (int even = 0; even < nums.length; even += 2) {
+
+    //         // If an even index contains an odd number,
+    //         // find an even number at an odd index
+    //         if (nums[even] % 2 != 0) {
+
+    //             while (nums[odd] % 2 != 0) {
+    //                 odd += 2;
+    //             }
+
+    //             // Swap the misplaced odd number
+    //             // with the misplaced even number
+    //             int temp = nums[even];
+    //             nums[even] = nums[odd];
+    //             nums[odd] = temp;
+    //         }
+    //     }
+
+    //     return nums;
+    // }
