e31abe1bd5e4527387ed8ac55a30497418dfb718
singammadhu <119906978+singammadhu@users.noreply.github.com>
2026-08-15 16:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (10.18%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,47 +1,26 @@
 class Solution {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
     public void sortColors(int[] nums) {
-       int n=nums.length;
-       int count0 = 0, count1 = 0, count2 = 0;
-       // Pass 1: Count occurrences of each number
-    for (int num : nums) {
-        if (num == 0) count0++;
-        else if (num == 1) count1++;
-        else if (num == 2) count2++;
+        int n=nums.length;
+       int low=0;
+       int mid =0;
+       int high=n-1;
+       while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums,mid,low);
+            low++;
+            mid++;
+        }else if(nums[mid]==1){
+            mid++;
+       }else{
+        swap(nums,high,mid);
+        high--;
+       }
     }
-
-    // Pass 2: Overwrite the array in sorted order
-    int idx = 0;
-    for (int i = 0; i < count0; i++) {
-        nums[idx++] = 0;
-    }
-    for (int i = 0; i < count1; i++) {
-        nums[idx++] = 1;
-    }
-    for (int i = 0; i < count2; i++) {
-        nums[idx++] = 2;
-    }
-
-    //   int zs=0;
-    //   int os=0;
-    //   int ts=0;
-    //   for(int i=0;i<nums.length;i++){
-    //     if(nums[i]==0){
-    //         zs++;
-    //     }else if(nums[i]==1){
-    //         os++;
-    //     }else{
-    //         ts++;
-    //     }
-    //   }
-    
-    //   for(int i=0; i<zs;i++){
-    //     nums[i]=0;
-    //   }
-    //   for(int i=zs; i<os+zs;i++){
-    //     nums[i]=1;
-    //   }
-    //   for(int i=zs+os; i<n;i++){
-    //     nums[i]=2;
-    //   }
-    }
+}
 }
